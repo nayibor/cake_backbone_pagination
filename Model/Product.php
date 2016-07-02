8d50cb9fbb7cee4a02bd0f20b9d3c52b1036abd1
Nuku Ameyibor <nayibor@gmail.com>
2016-07-02 05:05:07 +0000

    files needed for tutorial on cakephp+bakcbone pagination ready . have to test them out well and then write the tutorial out  .

---
+++ Model/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ * 
+ * Product Model 
+ */
+
+App::uses('AppModel', 'Model');
+
+
+class Product extends AppModel {
+
+    public $name = 'Product';
+   
+			
+			}
+    
+?>
+
